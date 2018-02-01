43b3bcd567a74306458ca1e6839fde8d305eb698
Pawel <2268686h@student.gla.ac.uk>
2018-02-01 18:04:01 +0000

    after chapter 9 problem with categories not sorted

--- rango/views.py
+++ rango/views.py
@@ -29,19 +29,19 @@ def about(request):
 
 
 def show_category(request, category_name_slug):
-        context_dict= {}
-
-        try:
-                category = Category.objects.get(slug=category_name_slug)
-                pages = Page.objects.filter(category=category)
-                context_dict['pages'] = pages
-                context_dict['category'] = category
-
-        except Category.DoesNotExist:
-                context_dict['category'] = None
-                context_dict['pages'] = None
-
-        return render(request, 'rango/category.html', context_dict)
+    context_dict = {}
+    try:
+        # can we find a category name slug matching?
+        # get() raises DoesNotExist exception if not
+        # get() returns one model instance or raises exception
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
 
 @login_required
 def add_category(request):
